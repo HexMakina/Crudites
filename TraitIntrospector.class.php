173544df34a8d57aacd05cf3f69678b07fa5addf
sam <sammy@hexmakina.be>
2021-07-30 22:14:18 +0200

    removed unused variable tab to spaces

--- TraitIntrospector.class.php
+++ TraitIntrospector.class.php
@@ -6,20 +6,20 @@ trait TraitIntrospector
   public function search_and_execute_trait_methods($method_name)
   {
     $errors = [];
-		$pattern = "Trait_$method_name"; // Trait Method must be correctly formatted
+    $pattern = "Trait_$method_name"; // Trait Method must be correctly formatted
     // vd("SEARCHING FOR ***$pattern " . get_class($this));
-		foreach((new \ReflectionClass($this))->getTraitNames() as $FQTraitName)
+    foreach((new \ReflectionClass($this))->getTraitNames() as $FQTraitName)
     {
-			foreach((new \ReflectionClass($FQTraitName))->getMethods() as $method)
+      foreach((new \ReflectionClass($FQTraitName))->getMethods() as $method)
       {
-				if(preg_match("/.+$pattern$/", $method->name, $match) === 1)
-				{
-					$callable = current($match);
-          $res = $this->$callable();
-					// TODO handle errors in callable..
-				}
-			}
-		}
-		return $errors;
+        if(preg_match("/.+$pattern$/", $method->name, $match) === 1)
+        {
+          $callable = current($match);
+          $this->$callable(); // $res = ? what to do with eventual return data?
+          // TODO handle errors in callable..
+        }
+      }
+    }
+    return $errors;
   }
 }
