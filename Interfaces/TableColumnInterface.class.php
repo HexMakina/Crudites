9dd9b104ddf050f453a422fd4f284e9693a7561b
sam <sammy@hexmakina.be>
2021-07-31 00:32:35 +0200

    Removed DEPRECATED function

--- Interfaces/TableColumnInterface.class.php
+++ Interfaces/TableColumnInterface.class.php
@@ -13,19 +13,6 @@ interface TableColumnInterface
   public function is_auto_incremented() : bool;
   public function is_nullable() : bool;
 
-  // public function is_boolean() : bool;
-  // public function is_integer() : bool;
-  // public function is_float() : bool;
-  //
-  // public function is_year() : bool;
-  // public function is_date_or_time() : bool;
-  //
-  // public function is_enum() : bool;
-  // public function enum_values() : array;
-
-  // public function is_text() : bool;
-  // public function is_string() : bool;
-
   public function default();
 
   public function foreign_table_name();
