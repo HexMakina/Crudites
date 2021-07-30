1b1988a392df318b5b386ab0c013d7f12aa4c3e8
sam <sammy@hexmakina.be>
2021-07-30 16:26:09 +0200

    indentation to whites spaces

--- Queries/BaseQuery.class.php
+++ Queries/BaseQuery.class.php
@@ -8,52 +8,52 @@ use \HexMakina\Crudites\Interfaces\TableManipulationInterface;
 abstract class BaseQuery
 {
 
-	static private $executions = 0;
+  static private $executions = 0;
 
-	const STATE_SUCCESS = '00000'; //PDO "error" code for "all is fine"
+  const STATE_SUCCESS = '00000'; //PDO "error" code for "all is fine"
 
-	const CODE_CREATE = 	'C';
-	const CODE_RETRIEVE = 'R';
-	const CODE_UPDATE = 	'U';
-	const CODE_DELETE = 	'D';
+  const CODE_CREATE =   'C';
+  const CODE_RETRIEVE = 'R';
+  const CODE_UPDATE =   'U';
+  const CODE_DELETE =   'D';
 
-	protected $database=null;
-	protected $table=null;
-	protected $statement=null;
-	protected $bindings=[];
+  protected $database=null;
+  protected $table=null;
+  protected $statement=null;
+  protected $bindings=[];
 
-	protected $connection = null;
-	protected $executed = false;
+  protected $connection = null;
+  protected $executed = false;
   protected $state = null;
 
-	protected $prepared_statement = null;
+  protected $prepared_statement = null;
 
-	protected $inserted_id = null;
-	protected $row_count = null;
+  protected $inserted_id = null;
+  protected $row_count = null;
 
   protected $error_code = null;
   protected $error_text = null;
-	//------------------------------------------------------------  DEBUG
-	public function __debugInfo()
-	{
-		$dbg = [];
+  //------------------------------------------------------------  DEBUG
+  public function __debugInfo()
+  {
+    $dbg = [];
     if(isset($table))
-		  $dbg['table_name()'] = $this->table_name();
+      $dbg['table_name()'] = $this->table_name();
 
     $dbg = array_merge($dbg, get_object_vars($this));
-		unset($dbg['table']);
+    unset($dbg['table']);
 
-		foreach($dbg as $k => $v)
-			if(!isset($dbg[$k]))
-				unset($dbg[$k]);
+    foreach($dbg as $k => $v)
+      if(!isset($dbg[$k]))
+        unset($dbg[$k]);
 
-		$dbg['bindings'] = json_encode($dbg['bindings']);
-		if(empty($this->bindings))
-			unset($dbg['bindings']);
+    $dbg['bindings'] = json_encode($dbg['bindings']);
+    if(empty($this->bindings))
+      unset($dbg['bindings']);
 
-		$dbg['statement()'] = $this->statement();
-		return $dbg;
-	}
+    $dbg['statement()'] = $this->statement();
+    return $dbg;
+  }
 
   public function __toString()
   {
@@ -61,204 +61,203 @@ abstract class BaseQuery
   }
 
 
-	abstract public function generate();
+  abstract public function generate();
 
-	// returns one of C, R, U, D
+  // returns one of C, R, U, D
   // TODO ? make them abstract ? forces knowledge of these methods..
-  public function is_create(){		return false;}
-  public function is_retrieve(){	return false;}
-  public function is_update(){		return false;}
-  public function is_delete(){		return false;}
+  public function is_create(){    return false;}
+  public function is_retrieve(){  return false;}
+  public function is_update(){    return false;}
+  public function is_delete(){    return false;}
 
   public function query_code()
-	{
-		if($this->is_create())				return self::CODE_CREATE;
-		elseif($this->is_retrieve())	return self::CODE_RETRIEVE;
-		elseif($this->is_update())		return self::CODE_UPDATE;
-		elseif($this->is_delete())		return self::CODE_DELETE;
+  {
+    if($this->is_create())        return self::CODE_CREATE;
+    elseif($this->is_retrieve())  return self::CODE_RETRIEVE;
+    elseif($this->is_update())    return self::CODE_UPDATE;
+    elseif($this->is_delete())    return self::CODE_DELETE;
 
-		throw new CruditesException('UNKOWN_QUERY_CODE');
+    throw new CruditesException('UNKOWN_QUERY_CODE');
   }
 
-	public function is_alteration(){return ($this->is_create() || $this->is_update() || $this->is_delete());}
+  public function is_alteration(){return ($this->is_create() || $this->is_update() || $this->is_delete());}
 
-	//------------------------------------------------------------  GET/SETTERS
-	public function statement($setter = null) : string
-	{
+  //------------------------------------------------------------  GET/SETTERS
+  public function statement($setter = null) : string
+  {
     if(!is_null($setter))
-			$this->statement = $setter;
+      $this->statement = $setter;
 
-		return $this->statement ?? $this->generate();
-	}
+    return $this->statement ?? $this->generate();
+  }
 
-	public function bindings($setter = null)
-	{
-		if(is_null($setter) || !is_array($setter))
-			return $this->bindings;
+  public function bindings($setter = null)
+  {
+    if(is_null($setter) || !is_array($setter))
+      return $this->bindings;
 
-		$this->bindings = $setter;
-		return $this;
-	}
+    $this->bindings = $setter;
+    return $this;
+  }
 
-	public function connection($setter = null)
-	{
-		if(is_null($setter))
-			return $this->connection;
+  public function connection($setter = null)
+  {
+    if(is_null($setter))
+      return $this->connection;
 
-		$this->connection = $setter;
-		return $this;
-	}
+    $this->connection = $setter;
+    return $this;
+  }
 
   public function has_table() : bool
   {
-		return !is_null($this->table);
+    return !is_null($this->table);
   }
 
   public function table(TableManipulationInterface $setter = null) : TableManipulationInterface
-	{
-		return is_null($setter) ? $this->table : ($this->table = $setter);
-	}
+  {
+    return is_null($setter) ? $this->table : ($this->table = $setter);
+  }
 
-	public function table_name()
-	{
-		return $this->table->name();
-	}
+  public function table_name()
+  {
+    return $this->table->name();
+  }
 
-	//------------------------------------------------------------  PREP::FIELDS
-	public function table_label($table_name=null)
-	{
-		return $table_name ?? $this->table_name();
-	}
+  //------------------------------------------------------------  PREP::FIELDS
+  public function table_label($table_name=null)
+  {
+    return $table_name ?? $this->table_name();
+  }
 
-	public function field_label($field, $table_name=null)
-	{
+  public function field_label($field, $table_name=null)
+  {
     if($table_name === '')
-			return "`$field`";
-		return sprintf('`%s`.`%s`', $this->table_label($table_name), $field);
-	}
+      return "`$field`";
+    return sprintf('`%s`.`%s`', $this->table_label($table_name), $field);
+  }
 
-	//------------------------------------------------------------  PREP::BINDINGS
-	public function bind_label($field, $table_name=null)
-	{
-		return ':'.$this->table_label($table_name).'_'.$field;
-	}
+  //------------------------------------------------------------  PREP::BINDINGS
+  public function bind_label($field, $table_name=null)
+  {
+    return ':'.$this->table_label($table_name).'_'.$field;
+  }
 
   public function bind($field, $value, $operator = '=', $table_name=null)
-	{
-		$binding_name = $this->bind_label($field, $table_name);
-		$this->bindings[$binding_name] = $value;
-		return $this->field_label($field, $table_name)." $operator $binding_name";
-	}
+  {
+    $binding_name = $this->bind_label($field, $table_name);
+    $this->bindings[$binding_name] = $value;
+    return $this->field_label($field, $table_name)." $operator $binding_name";
+  }
 
   public function bind_name($table_name, $field, $value, $bind_label=null)
-	{
-		$bind_label = $bind_label ?? $this->bind_label($field, $table_name);
+  {
+    $bind_label = $bind_label ?? $this->bind_label($field, $table_name);
 
-		$this->bindings[$bind_label] = $value;
+    $this->bindings[$bind_label] = $value;
 
-		return $bind_label;
-	}
+    return $bind_label;
+  }
 
-	//------------------------------------------------------------  Run
+  //------------------------------------------------------------  Run
   // throws CruditesException on failure
   // returns itself
-	// DEBUG dies on \Exception
-	public function run()
-	{
+  // DEBUG dies on \Exception
+
+  public function run()
+  {
+    if(is_null($this->connection()))
+      throw new CruditesException('NO_CONNECTION');
     try
-		{
-			if(is_null($this->connection))
-				throw new CruditesException('NO_CONNECTION');
+    {
+      if(!$this->is_prepared())
+        $this->prepared_statement = $this->connection->prepare($this->statement());
 
-			if(!$this->is_prepared())
-				$this->prepared_statement = $this->connection->prepare($this->statement());
-
-			if($this->prepared_statement->execute($this->bindings()) !== false) // execute returns TRUE on success or FALSE on failure.
+      if($this->prepared_statement->execute($this->bindings()) !== false) // execute returns TRUE on success or FALSE on failure.
       {
-				++self::$executions;
+        ++self::$executions;
 
-				// vdt($this->statement(), 'EXEC #'.self::$executions); if(!empty($this->bindings())) vd($this->bindings(), 'bindings');
+        $this->is_executed(true);
 
-				$this->is_executed(true);
-
-				if($this->prepared_statement->errorCode() === self::STATE_SUCCESS)
+        if($this->prepared_statement->errorCode() === self::STATE_SUCCESS)
         {
-					$this->state = self::STATE_SUCCESS;
-					$this->row_count = $this->prepared_statement->rowCount();
+          $this->state = self::STATE_SUCCESS;
+          $this->row_count = $this->prepared_statement->rowCount();
 
-					if($this->is_create())
-          	$this->inserted_id = $this->connection->lastInsertId();
-		    }
+          if($this->is_create())
+            $this->inserted_id = $this->connection->lastInsertId();
+        }
       }
     }
-		catch (\PDOException $e)
-		{
-			throw (new CruditesException($e->getMessage()))->fromQuery($this);
+    catch (\PDOException $e)
+    {
+      throw (new CruditesException($e->getMessage()))->fromQuery($this);
     }
-		catch(\Exception $e)
-		{
-			vd(get_class($e));
-			ddt($e);
-		}
+    // not doing anything with it.. let it blow
+    // catch(\Exception $e)
+    // {
+    //   var_dump(get_class($e));
+    //   var_dump($e);
+    //   die;
+    // }
 
-		return $this;
-	}
+    return $this;
+  }
 
-	//------------------------------------------------------------  Return
-	public function ret($mode=null, $option=null)
-	{
-		if(!$this->is_executed())
-		{
-			try{$this->run();}
-			catch(CruditesException $e){return false;}
-		}
-		if(!$this->is_success())
-			return false;
+  //------------------------------------------------------------  Return
+  public function ret($mode=null, $option=null)
+  {
+    if(!$this->is_executed())
+    {
+      try{$this->run();}
+      catch(CruditesException $e){return false;}
+    }
+    if(!$this->is_success())
+      return false;
 
-    if(is_null($mode)) // nothin was specified, it's probe-time
-		{
-			if($this->has_table() && !is_null($class_name = $this->table()->map_class()))
-			{
-				$mode = \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE;
-				$option = $class_name;
-			}
-			else
-				$mode = \PDO::FETCH_ASSOC;
-		}
-		return is_null($option) ? $this->prepared_statement->fetchAll($mode) : $this->prepared_statement->fetchAll($mode, $option);
-	}
+    // if(is_null($mode)) // nothin was specified, it's probe-time
+    // {
+    //   if($this->has_table() && !is_null($class_name = $this->table()->map_class()))
+    //   {
+    //     $mode = \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE;
+    //     $option = $class_name;
+    //   }
+    //   else
+    //     $mode = \PDO::FETCH_ASSOC;
+    // }
+    return is_null($option) ? $this->prepared_statement->fetchAll($mode) : $this->prepared_statement->fetchAll($mode, $option);
+  }
 
-	//------------------------------------------------------------ Return:count
-	public function count()
-	{
-		if(!$this->is_executed())
-			try{$this->run();}
-			catch(CruditesException $e){vdt($e);return null;}
+  //------------------------------------------------------------ Return:count
+  public function count()
+  {
+    if(!$this->is_executed())
+      $this->run();
 
-		return $this->is_success() ? $this->row_count : null;
-	}
+    return $this->is_success() ? $this->row_count : null;
+  }
 
-	//------------------------------------------------------------  Status
-	public function is_prepared()
-	{
-		return !is_null($this->prepared_statement) && false !== $this->prepared_statement;
-	}
+  //------------------------------------------------------------  Status
+  public function is_prepared()
+  {
+    return !is_null($this->prepared_statement) && false !== $this->prepared_statement;
+  }
 
-	public function is_executed($setter=null)
-	{
-		return is_null($setter) ? $this->executed === true : ($this->executed = $setter);
-	}
+  public function is_executed($setter=null)
+  {
+    return is_null($setter) ? $this->executed === true : ($this->executed = $setter);
+  }
 
-	public function is_success()
-	{
-		return $this->state === self::STATE_SUCCESS;
-	}
+  public function is_success()
+  {
+    return $this->state === self::STATE_SUCCESS;
+  }
 
-	public function error_info()
-	{
-		if($this->is_prepared())
-			return $this->prepared_statement->errorInfo();
+  public function error_info()
+  {
+    if($this->is_prepared())
+      return $this->prepared_statement->errorInfo();
 
-		return $this->connection->errorInfo();
-	}
+    return $this->connection->errorInfo();
+  }
 }
