348d8b2a0ce75fb90c9f4a9232f4363f2199a937
sam <sammy@hexmakina.be>
2021-09-07 03:34:46 +0200

    added: interfaces instead of classes

--- Table/Manipulation.php
+++ Table/Manipulation.php
@@ -4,6 +4,8 @@ namespace HexMakina\Crudites\Table;
 
 use HexMakina\Interfaces\Database\TableManipulationInterface;
 use HexMakina\Interfaces\Database\SelectInterface;
+use HexMakina\Interfaces\Database\QueryInterface;
+use HexMakina\Interfaces\Database\RowInterface;
 use HexMakina\Crudites\Queries\Select;
 use HexMakina\Crudites\Queries\Insert;
 use HexMakina\Crudites\Queries\Update;
@@ -13,33 +15,33 @@ class Manipulation extends Description implements TableManipulationInterface
 {
 
     // creates a new Row based on the table
-    public function produce($dat_ass = []): Row
+    public function produce($dat_ass = []): RowInterface
     {
         return new Row($this, $dat_ass);
     }
 
-    public function restore($dat_ass): Row
+    public function restore($dat_ass): RowInterface
     {
         $row = new Row($this);
         $row->load($dat_ass);
         return $row;
     }
 
-    public function insert($values = []): Insert
+    public function insert($values = []): QueryInterface
     {
         $q = new Insert($this, $values);
         $q->connection($this->connection());
         return $q;
     }
 
-    public function update($modifications = [], $conditions = []): Update
+    public function update($modifications = [], $conditions = []): QueryInterface
     {
         $q = new Update($this, $modifications, $conditions);
         $q->connection($this->connection());
         return $q;
     }
 
-    public function delete($conditions = []): Delete
+    public function delete($conditions = []): QueryInterface
     {
         $q = new Delete($this, $conditions);
         $q->connection($this->connection());
