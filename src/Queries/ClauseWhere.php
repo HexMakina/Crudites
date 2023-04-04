a308454ee442abba166608674a42ea4ec7c52074
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-04-04 13:27:24 +0200

    cleanup

--- src/Queries/ClauseWhere.php
+++ src/Queries/ClauseWhere.php
@@ -69,32 +69,32 @@ trait ClauseWhere
 
     public function whereEQ($field, $value, $table_name = null, $bindname = null)
     {
-               return $this->whereBindField($table_name, $field, self::$OP_EQ, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_EQ, $value, $bindname);
     }
 
     public function whereGT($field, $value, $table_name = null, $bindname = null)
     {
-               return $this->whereBindField($table_name, $field, self::$OP_GT, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_GT, $value, $bindname);
     }
 
     public function whereLT($field, $value, $table_name = null, $bindname = null)
     {
-               return $this->whereBindField($table_name, $field, self::$OP_LT, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_LT, $value, $bindname);
     }
 
     public function whereGTE($field, $value, $table_name = null, $bindname = null)
     {
-               return $this->whereBindField($table_name, $field, self::$OP_GTE, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_GTE, $value, $bindname);
     }
 
     public function whereLTE($field, $value, $table_name = null, $bindname = null)
     {
-              return $this->whereBindField($table_name, $field, self::$OP_LTE, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_LTE, $value, $bindname);
     }
 
     public function whereNotEQ($field, $value, $table_name = null, $bindname = null)
     {
-          return $this->whereBindField($table_name, $field, self::$OP_NEQ, $value, $bindname);
+        return $this->whereBindField($table_name, $field, self::$OP_NEQ, $value, $bindname);
     }
 
     public function wherePrimary($pk_values)
@@ -194,7 +194,7 @@ trait ClauseWhere
      * @param $filters_operator Object, inclusive or exclusive search
      */
 
-     // sub array filters[$content]
+    // sub array filters[$content]
     public function whereFilterContent(array $filters_content, $search_table = null, $filters_operator = null)
     {
         if (!isset($filters_content['term'])) {
@@ -222,11 +222,11 @@ trait ClauseWhere
             $search_field = $this->backTick($search_field, $search_table);
 
             if ($search_mode === self::$OP_EQ) {
-                $content_wc [] = sprintf('%s = \'%s\' ', $search_field, $search_term); // TODO bindthis
+                $content_wc[] = sprintf('%s = \'%s\' ', $search_field, $search_term); // TODO bindthis
             } else // %%
             {
                 $pattern = str_replace('TERM', $search_term, $search_mode);
-                $content_wc [] = sprintf(' %s LIKE \'%s\' ', $search_field, $pattern); // TODO bindthis
+                $content_wc[] = sprintf(' %s LIKE \'%s\' ', $search_field, $pattern); // TODO bindthis
             }
         }
 
