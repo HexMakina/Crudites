22a3d0af4a9cdaa9a12b2afa257f7bda1b51d754
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-05-09 22:12:03 +0200

    adapted to PreparedQueryInterface

--- src/Queries/Select.php
+++ src/Queries/Select.php
@@ -6,7 +6,7 @@ use HexMakina\BlackBox\Database\TableInterface;
 use HexMakina\BlackBox\Database\SelectInterface;
 use HexMakina\Crudites\CruditesException;
 
-class Select extends BaseQuery implements SelectInterface
+class Select extends BaseQuery implements PreparedQueryInterface
 {
     use ClauseJoin;
     use ClauseWhere;
