493e2d026387274205f118e8dd772bc25b63887b
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-17 22:33:44 +0200

    new orderBy() param format, new select columns generation

--- src/Queries/Select.php
+++ src/Queries/Select.php
@@ -10,6 +10,7 @@ class Select extends PreparedQuery implements SelectInterface
     use ClauseJoin;
     use ClauseWhere;
 
+    protected $columns;
     protected $limit;
 
     protected $limit_number;
@@ -22,8 +23,7 @@ class Select extends PreparedQuery implements SelectInterface
         $this->table = $table;
         $this->table_alias = $table_alias;
 
-        $columns = is_array($columns) ? $columns : (is_string($columns) ? explode(',', $columns) : []);
-        $this->setClause('select', $columns);
+        $this->columns($columns);
     }
 
     public function tableLabel($forced_value = null)
@@ -31,26 +31,63 @@ class Select extends PreparedQuery implements SelectInterface
         return $forced_value ?? $this->table_alias ?? $this->tableName();
     }
 
-    public function columns($setter = null)
+    public function columns($setter = null): array
     {
-        if (is_null($setter)) {
-            return $this->clause('select');
+        if (!is_null($setter))
+        {
+            $this->columns = [];
+            $this->selectAlso($setter);
         }
+        
+        return $this->columns ?? [];
+    }
 
-        if (is_array($setter)) {
-            $this->setClause('select', $setter);
+    public function selectAlso($setter): self
+    {
+        if (is_null($setter))
+            return $this->columns;
+
+
+        if (is_string($setter))
+            $setter = explode(',', $setter);
+
+
+        if (!is_array($setter))
+            $setter = [];
+
+        foreach ($setter as $alias => $columnInfo) {
+            
+            $table = $this->tableLabel();
+            $column = null;
+            
+            if (is_int($alias)) {
+                $column = $columnInfo;
+                $alias = null;
+
+            } elseif (is_array($columnInfo)) {
+                if (isset($columnInfo[1])) {
+                    [$table, $column] = $columnInfo;
+                } else {
+                    $table = -1;
+                    $column = $columnInfo[0];
+                }
+            }
+            else {
+                $column = $columnInfo;
+            }
+
+            $this->addColumn($column, $alias, $table);
         }
 
         return $this;
     }
 
-    public function selectAlso($setter): self
+    public function addColumn(string $name, string $alias = null, string $table = null)
     {
-        if(!is_array($setter)){
-            $setter = [$setter];
-        }
-        $res = $this->setClause('select', array_unique(array_merge($this->clause('select'), $setter)));
-        return $res;
+        if(empty($table))
+            $table = -1;
+
+        $this->columns[$table][] = empty($alias) ? $name : [$name, $alias];
     }
 
     public function groupBy($clause): self
@@ -58,11 +95,9 @@ class Select extends PreparedQuery implements SelectInterface
         $groupBy = null;
         if (is_string($clause)) {
             $groupBy = $this->backTick($clause, $this->tableLabel());
-
         } elseif (is_array($clause)) {
             if (isset($clause[1])) { // 0: table, 1: field            
                 $groupBy = $this->backTick($clause[1], $clause[0]);
-            
             } else { // 0: field            
                 $groupBy = $this->backTick($clause[0], null);
             }
@@ -76,28 +111,29 @@ class Select extends PreparedQuery implements SelectInterface
         return $this->addClause('having', $condition);
     }
 
+    /**
+     * @param $clause, cannot be empty, must be an array or a string
+     * 
+     * if array, it must adhere to this format
+     *       [0] => field
+     *       [1] => direction (null, default ASC)
+     *       [2] => table (null, default tableLabel())
+     */ 
     public function orderBy($clause): self
     {
-        if (is_array($clause) && count($clause) > 1) {
+        if(empty($clause) || (!is_array($clause) && !is_string($clause)))
+            throw new \InvalidArgumentException('ORDER_BY_INVALID_CLAUSE');
 
-            if (isset($clause[2])) { // 0:table, 1:field, 2:direction
-                $column = $this->backTick($clause[1], $clause[0]);
-            } 
-            elseif(isset($clause[1]) || isset($clause[0])) { // 0: field, 1: direction
-                $column = $this->backTick($clause[0], $this->tableLabel());
-            }
+        if (is_array($clause)) {
 
-            if(!isset($clause[1])){ // 0:field, direction ASC
-                array_push($clause, 'ASC');
-            }
-
-            $direction = array_pop($clause);
-
-            $clause = sprintf('%s %s', $column, $direction);
+            $column     = $clause[0];
+            $direction  = $clause[1] ?? 'ASC';
+            $table      = $clause[2] ?? $this->tableLabel();
+            
+            $clause =  sprintf('%s %s', $this->backTick($column, $table), $direction);
         }
 
-        if(is_string($clause))
-            $this->addClause('order', $clause);
+        $this->addClause('orderBy', $clause);
 
         return $this;
     }
@@ -118,9 +154,11 @@ class Select extends PreparedQuery implements SelectInterface
 
         $this->table_alias ??= '';
 
-        $query_fields = empty($this->clause('select')) ? ['*'] : $this->clause('select');
-        $ret = PHP_EOL . 'SELECT ' . implode(', ' . PHP_EOL, $query_fields);
-        $ret .= PHP_EOL . sprintf(' FROM `%s` %s ', $this->tableName(), $this->table_alias);
+        $ret = PHP_EOL . 'SELECT ' . implode(', ' . PHP_EOL, $this->generateSelectColumns());
+
+        $ret .= PHP_EOL . sprintf(' FROM `%s`', $this->tableLabel());
+        if ($this->tableName() !== $this->table_alias)
+            $ret .= ' ' . $this->table_alias;
 
         if (!empty($this->clause('join'))) {
             $ret .= PHP_EOL . ' ' . implode(PHP_EOL . ' ', $this->clause('join'));
@@ -128,23 +166,56 @@ class Select extends PreparedQuery implements SelectInterface
 
         $ret .= $this->generateWhere();
 
-        
-        foreach (['group' => 'GROUP BY', 'having' => 'HAVING', 'order' => 'ORDER BY'] as $part => $prefix) {
+
+        foreach (['group' => 'GROUP BY', 'having' => 'HAVING', 'orderBy' => 'ORDER BY'] as $part => $prefix) {
             if (!empty($this->clause($part))) {
                 $ret .= PHP_EOL . sprintf(' %s ', $prefix) . implode(', ', $this->clause($part));
             }
         }
 
-
-
         if (!empty($this->limit_number)) {
-            $offset = $this->limit_offset ?? 0;
-            $number = $this->limit_number;
-            $ret .= PHP_EOL . sprintf(' LIMIT %s OFFSET %s', $number, $offset);
+            $ret .= PHP_EOL . sprintf(' LIMIT %s OFFSET %s', $this->limit_number, $this->limit_offset ?? 0);
         }
+
+
         return $ret;
     }
 
+    private function generateSelectColumns()
+    {
+        $query_fields = $this->columns();
+
+        $ticked = [];
+
+        foreach ($query_fields as $table => $columns) {
+
+            if(in_array('*', $columns)) {
+                $ticked [] = sprintf('`%s`.*', $this->tableLabel(), '*');
+                continue;
+            }
+            elseif(is_int($table)){
+                // GROUP_CONCAT, SUM, COUNT, etc
+                foreach($columns as $columnInfo){
+                    $ticked [] = sprintf('%s AS `%s`', $columnInfo[0], $columnInfo[1]);
+            }
+            }
+            else{
+                foreach($columns as $columnInfo){
+                    // do we have an alias ?
+                    if(is_array($columnInfo)){
+                        [$column, $alias] = $columnInfo;
+                        $ticked [] = sprintf('%s AS `%s`', $this->backTick($column, $table), $alias);
+
+                    }
+                    else{
+                        $ticked [] = $this->backTick($columnInfo, $table);
+                    }
+                }
+            }
+        }
+
+        return $ticked;
+    }
     //------------------------------------------------------------ SELECT:FETCHING RESULT
 
     public function retObj($c = null)
