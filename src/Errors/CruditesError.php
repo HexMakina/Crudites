15de3d4477058c611c3108ba96d281c9214f60bb
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-22 16:21:18 +0100

    migrating towards unified error class

--- src/Errors/CruditesError.php
+++ src/Errors/CruditesError.php
@@ -1,15 +1,18 @@
 <?php
 namespace HexMakina\Crudites\Errors;
 
-class CruditesError {
+class CruditesError 
+{
+    protected string $message;
+    protected string $state;
+    protected int $code;
 
     protected string $table;
     protected array $columns = [];
     protected string $constraint;
-    protected string $message;
-    protected int $sqlErrorCode;
-
-    public function __construct(string $message) {
+    
+    public function __construct($message)
+    {
         $this->message = $message;
     }
 
@@ -23,6 +26,56 @@ class CruditesError {
 
         return $ret;
     }
+
+    public function import($something_PDO): self
+    {
+        $errorInfo = null;
+
+        if(method_exists($something_PDO, 'errorInfo'))
+            $errorInfo = $something_PDO->errorInfo();
+        elseif(property_exists($something_PDO, 'errorInfo'))
+            $errorInfo = $something_PDO->errorInfo;
+        else
+            throw new \InvalidArgumentException('INVALID_PDO_CLASS');
+
+        // 0: the SQLSTATE associated with the last operation on the database handle
+        $this->setState($errorInfo[0] ?? null);
+
+        // 1: driver-specific error code.
+        $this->setCode($errorInfo[1] ?? null);
+
+        // 2: driver-specific error message
+        $this->setMessage($errorInfo[2] ?? null);
+
+        return $this;
+    }
+
+
+    public function setState(string $state): void {
+        $this->state = $state;
+    }
+
+    public function getState(): string {
+        return $this->state;
+    }
+
+    public function setCode(int $code): void {
+        $this->code = $code;
+    }
+
+    public function getCode(): int {
+        return $this->code;
+    }
+    
+
+    public function setMessage(string $message): void {
+        $this->message = $message;
+    }
+
+    public function getMessage(): string {
+        return $this->message;
+    }
+
 
     public function setTable(string $table): void {
         $this->table = $table;
@@ -46,21 +99,5 @@ class CruditesError {
 
     public function getConstraint(): string {
         return $this->constraint;
-    }
-
-    public function setMessage(string $message): void {
-        $this->message = $message;
-    }
-
-    public function getUserMessage(): string {
-        return $this->message;
-    }
-
-    public function setSqlErrorCode(int $sqlErrorCode): void {
-        $this->sqlErrorCode = $sqlErrorCode;
-    }
-
-    public function getSqlErrorCode(): int {
-        return $this->sqlErrorCode;
     }
 }
