9cbce37626df91b1595fcf61e579d113dfee60a6
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-11-23 17:20:18 +0100

    moved high level comment to interface

--- src/Schema.php
+++ src/Schema.php
@@ -23,33 +23,21 @@ class Schema implements SchemaInterface
         $this->tables = SchemaLoader::load($connection);
     }
 
-    /**
-     * Checks if the specified table exists.
-     * 
-     * @param string $table The name of the table.
-     * @return bool True if the table exists, false otherwise.
-     */
+    public function connection(): ConnectionInterface
+    {
+        return $this->connection;
+    }
+
     public function hasTable(string $table): bool
     {
         return isset($this->tables[$table]);
     }
 
-    /**
-     * Retrieves the names of the tables in the database.
-     *
-     * @return array<string> The names of the tables.
-     */
     public function tables(): array
     {
         return array_keys($this->tables);
     }
 
-    /**
-     * Retrieves the auto-incremented primary key column for the specified table.
-     * 
-     * @param string $table The name of the table.
-     * @return string|null The name of the auto-incremented primary key column, or null if the table has no auto-incremented primary key.
-     */
     public function autoIncrementedPrimaryKey(string $table): ?string
     {
         foreach ($this->primaryKeys($table) as $column) {
@@ -59,70 +47,31 @@ class Schema implements SchemaInterface
         return null;
     }
 
-    /**
-     * Retrieves the primary key columns for the specified table.
-     *
-     * @param string $table The name of the table.
-     * @return array<int,string> An array of column names that are part of the primary key.
-     */
     public function primaryKeys(string $table): array
     {
         return $this->hasTable($table) ? $this->tables[$table]['primary'] : [];
     }
 
-    /**
-     * Retrieves the foreign key columns for the specified table.
-     *
-     * @param string $table The name of the table.
-     * @return array<string,array> An array of foreign references [table, column], indexed by column name.
-     */
     public function foreignKeys(string $table): array
     {
         return $this->hasTable($table) ? $this->tables[$table]['foreign'] : [];
     }
 
-    /**
-     * Retrieves the foreign key columns for the specified table and column.
-     *
-     * @param string $table The name of the table.
-     * @param string $column The name of the column.
-     * @return array<string,array> An array [table, column] of the foreign reference.
-     */
     public function foreignKey(string $table, string $column): array
     {
         return $this->hasTable($table) ? ($this->tables[$table]['foreign'][$column] ?? []) : [];
     }
 
-    /**
-     * Retrieves the unique constraints for the specified table.
-     *
-     * @param string $table The table name.
-     * @return array The sets of unique columns, indexed by constraint name.
-     */
     public function uniqueKeys(string $table): array
     {
         return $this->hasTable($table) ? $this->tables[$table]['unique'] : [];
     }
 
-    /**
-     * Retrieves the unique columns for the specified table and constraint name.
-     *
-     * @param string $table The name of the table.
-     * @param string $constraint The name of the constraint.
-     * @return array The unique columns for the specified constraint.
-     */
     public function uniqueColumnsByName(string $table, string $constraint): array
     {
         return $this->hasTable($table) ? $this->tables[$table]['unique'][$constraint] : [];
     }
 
-    /**
-     * Retrieves the unique constraints for the specified table and column.
-     *
-     * @param string $table The name of the table.
-     * @param string $column The name of the column.
-     * @return array The sets of unique constraints columns the column belongs to, indexed by constraint name.
-     */
     public function uniqueColumnsFor(string $table, string $column): array
     {
         $ret = [];
@@ -136,37 +85,16 @@ class Schema implements SchemaInterface
         return $ret;
     }
 
-    /**
-     * Checks if the specified column exists in the table.
-     *
-     * @param string $table The name of the table.
-     * @param string $column The name of the column.
-     * @return bool True if the column exists in the table, false otherwise.
-     */
     public function hasColumn(string $table, string $column): bool
     {
         return $this->hasTable($table) && !empty($this->tables[$table]['columns'][$column]);
     }
 
-    /**
-     * Retrieves the names of the columns in the specified table.
-     *
-     * @param string $table The name of the table.
-     * @return array<string> The names of the columns in the table.
-     */
     public function columns(string $table): array
     {
         return $this->hasTable($table) ? array_keys($this->tables[$table]['columns']) : [];
     }
 
-    /**
-     * Retrieves the schema information for the specified column in the table.
-     *
-     * @param string $table The name of the table.
-     * @param string $column The name of the column.
-     * @return array The schema information for the column.
-     * @throws \InvalidArgumentException If the column does not exist in the table.
-     */
     public function column(string $table, string $column): array
     {
         if(!$this->hasColumn($table, $column)){
@@ -176,25 +104,11 @@ class Schema implements SchemaInterface
         return $this->tables[$table]['columns'][$column]['schema'] ?? throw new \InvalidArgumentException("ERR_MISSING_COLUMN_SCHEMA");
     }
 
-    /**
-     * Retrieves the attributes for the specified column in the table.
-     *
-     * @param string $table The name of the table.
-     * @param string $column The name of the column.
-     * @return SchemaAttributeInterface The attributes of the column.
-     */
     public function attributes(string $table, string $column): SchemaAttributeInterface
     {
         return new SchemaAttribute($this, $table, $column);
     }
 
-    /**
-     * Creates an insert query for the specified table with the given data.
-     *
-     * @param string $table The name of the table.
-     * @param array $dat_ass The data to insert.
-     * @return QueryInterface The insert query.
-     */
     public function insert(string $table, array $dat_ass): QueryInterface
     {
         $insert = new Insert($table, $this->filterData($table, $dat_ass));
@@ -203,14 +117,7 @@ class Schema implements SchemaInterface
         return $insert;
     }
 
-    /**
-     * Creates an update query for the specified table with the given alterations and conditions.
-     *
-     * @param string $table The name of the table.
-     * @param array<string,mixed> $alterations The data to update.
-     * @param array<string,mixed> $conditions The conditions for the update.
-     * @return QueryInterface The update query.
-     */
+
     public function update(string $table, array $alterations = [], array $conditions = []): QueryInterface
     {
         $update = new Update($table, $this->filterData($table, $alterations), $this->filterData($table, $conditions));
@@ -219,13 +126,6 @@ class Schema implements SchemaInterface
         return $update;
     }
 
-    /**
-     * Creates a delete query for the specified table with the given conditions.
-     *
-     * @param string $table The name of the table.
-     * @param array<string,mixed> $conditions The conditions for the delete.
-     * @return QueryInterface The delete query.
-     */
     public function delete(string $table, array $conditions): QueryInterface
     {
         $delete = new Delete($table, $this->filterData($table, $conditions));
@@ -234,14 +134,7 @@ class Schema implements SchemaInterface
         return $delete;
     }
 
-    /**
-     * Creates a select query for the specified table with the given columns and table alias.
-     *
-     * @param string $table The name of the table.
-     * @param array|null $columns The columns to select.
-     * @param string|null $table_alias The alias for the table.
-     * @return SelectInterface The select query.
-     */
+
     public function select(string $table, array $columns = null, string $table_alias = null): SelectInterface
     {
         $table_alias ??= $table;
@@ -252,25 +145,11 @@ class Schema implements SchemaInterface
         return $select;
     }
 
-    /**
-     * Matches the uniqueness constraints for the specified table with the given data.
-     *
-     * @param string $table The name of the table.
-     * @param array $dat_ass The data to match.
-     * @return array The matched uniqueness constraints.
-     */
     public function matchUniqueness(string $table, array $dat_ass): array
     {
         return $this->matchPrimaryKeys($table, $dat_ass) ?? $this->matchUniqueKeys($table, $dat_ass) ?? [];
     }
 
-    /**
-     * Matches the primary key constraints for the specified table with the given data.
-     *
-     * @param string $table The name of the table.
-     * @param array $dat_ass The data to match.
-     * @return array|null The matched primary key constraints, or null if no match is found.
-     */
     public function matchPrimaryKeys(string $table, array $dat_ass): ?array
     {
         $primaryKeys = $this->primaryKeys($table);
@@ -279,13 +158,6 @@ class Schema implements SchemaInterface
         return count($match) === count($primaryKeys) ? $match : null;
     }
 
-    /**
-     * Matches the unique key constraints for the specified table with the given data.
-     *
-     * @param string $table The name of the table.
-     * @param array $dat_ass The data to match.
-     * @return array|null The matched unique key constraints, or null if no match is found.
-     */
     public function matchUniqueKeys(string $table, array $dat_ass): ?array
     {
         foreach ($this->uniqueKeys($table) as $constraint => $columns) {
