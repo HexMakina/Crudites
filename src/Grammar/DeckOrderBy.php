ed2c3520c877453a6f9d07eb82386b513223bf73
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-12-05 19:30:23 +0100

    using predicate to format

--- src/Grammar/DeckOrderBy.php
+++ src/Grammar/DeckOrderBy.php
@@ -8,9 +8,6 @@ class DeckOrderBy extends Deck
     // Deck uses AS for alias, DeckOrderBy uses ASC/DESC for direction
     protected function format($aggregate, string $direction = null): string
     {
-        $ret = is_string($aggregate) ? $aggregate : self::identifier($aggregate);
-        $ret .= ' ' . in_array($direction, ['ASC', 'DESC']) ? $direction : 'ASC';
-
-        return $ret;
+        return (string)(new Predicate($aggregate, $direction));
     }
 }
