0025100ed0bc14d221489e541029a14d5b15cd02
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-12-06 01:48:04 +0100

    refactor identifier method to support custom tick characters and improve handling of array references

--- src/Grammar/Grammar.php
+++ src/Grammar/Grammar.php
@@ -5,25 +5,20 @@ namespace HexMakina\Crudites\Grammar;
 class Grammar
 {
     /**
-     * Adds backticks to a reference.
-     *
-     * @param $reference The reference to add backticks to.
-     * @return string The reference with backticks added.
+     * Surrounds a string with backticks by default
+     * @param string|array $reference : a string or an array with 2 elements, the first being the table name and the second the column name
+     * @param string $tick : the character to use for surrounding the string
+     * @return string : the string surrounded by the tick character
+     * 
      */
-
-    public static function rawOrBacktick($reference): string
-    {
-        return is_string($reference) ? $reference : self::identifier($reference);
-    }
-
-    public static function identifier($reference): string
+    public static function identifier($reference, $tick='`'): string
     {
         if (is_array($reference)) {
-            $tick = $reference[0];
+            $identifier = $reference[0];
             if (isset($reference[1])) {
-                $tick = $tick . '`.`' . $reference[1];
+                $identifier = $identifier.$tick . '.' . $tick.$reference[1];
             }
-            $reference = $tick;
+            $reference = $identifier;
         }
 
         return sprintf('`%s`', $reference);
