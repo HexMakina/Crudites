1f8ab9c40fd49302d970022445c9c9115f08792c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-03-10 00:59:08 +0100

    refactor HasOne relation methods to return boolean and utilize Result class for query execution

--- src/Relation/HasOne.php
+++ src/Relation/HasOne.php
@@ -3,6 +3,7 @@
 namespace HexMakina\Crudites\Relation;
 
 use HexMakina\BlackBox\Database\ConnectionInterface;
+use HexMakina\Crudites\Result;
 
 class HasOne extends AbstractRelation
 {
@@ -17,20 +18,24 @@ class HasOne extends AbstractRelation
         $this->propertiesFromJoin($join);
     }
 
-    protected function propertiesFromJoin($join){
+    protected function propertiesFromJoin($join)
+    {
         $this->primary_col = array_keys($join);
         $this->primary_col = array_pop($this->primary_col);
         [$this->secondary_table, $this->secondary_col] = $join[$this->primary_col];
     }
 
-    public function link(int $primary_id, $secondary_id){
-        $table =$this->connection->schema()->table($this->primary_table);
-        $table->insert($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
+    public function link(int $primary_id, $secondary_id): bool
+    {
+        $query = $this->connection->schema()->insert($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
+        $res = new Result($this->connection->pdo(), $query);
+        return $res->ran();
     }
 
-    public function unlink(int $primary_id, $secondary_id){
-        $table =$this->connection->schema()->table($this->primary_table);
-        $table->delete($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
-
+    public function unlink(int $primary_id, $secondary_id): bool
+    {
+        $query = $this->connection->schema()->delete($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
+        $res = new Result($this->connection->pdo(), $query);
+        return $res->ran();
     }
 }
