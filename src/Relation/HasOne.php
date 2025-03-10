10579db4f6bd5e57801ccb4c7b32ba5b18de22ef
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-03-10 01:05:58 +0100

    refactor HasOne relation methods to enforce type safety and simplify data handling

--- src/Relation/HasOne.php
+++ src/Relation/HasOne.php
@@ -9,33 +9,38 @@ class HasOne extends AbstractRelation
 {
     public const NAME = 'hasOne';
 
-    public function __construct($table, $join, ConnectionInterface $c)
+    public function __construct(string $table, array $join, ConnectionInterface $c)
     {
         $this->setConnection($c);
-
         $this->primary_table = $table;
-
         $this->propertiesFromJoin($join);
     }
 
-    protected function propertiesFromJoin($join)
+    protected function propertiesFromJoin(array $join): void
     {
-        $this->primary_col = array_keys($join);
-        $this->primary_col = array_pop($this->primary_col);
+        $this->primary_col = array_key_first($join);
         [$this->secondary_table, $this->secondary_col] = $join[$this->primary_col];
     }
 
     public function link(int $primary_id, $secondary_id): bool
     {
-        $query = $this->connection->schema()->insert($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
-        $res = new Result($this->connection->pdo(), $query);
-        return $res->ran();
+        $data = [
+            $this->primary_col => $primary_id,
+            $this->secondary_col => $secondary_id
+        ];
+
+        $query = $this->connection->schema()->insert($this->primary_table, $data);
+        return (new Result($this->connection->pdo(), $query))->ran();
     }
 
     public function unlink(int $primary_id, $secondary_id): bool
     {
-        $query = $this->connection->schema()->delete($this->primary_table, [$this->primary_col => $primary_id, $this->secondary_col => $secondary_id]);
-        $res = new Result($this->connection->pdo(), $query);
-        return $res->ran();
+        $criteria = [
+            $this->primary_col => $primary_id,
+            $this->secondary_col => $secondary_id
+        ];
+
+        $query = $this->connection->schema()->delete($this->primary_table, $criteria);
+        return (new Result($this->connection->pdo(), $query))->ran();
     }
 }
